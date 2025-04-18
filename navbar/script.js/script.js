ef387f0a2fe69b85741ab406a5d7e07b70feff14
Jorge Ramirez <46687749+jorgeramirez2018@users.noreply.github.com>
2025-04-18 12:27:58 -0500

    Delete navbar directory

--- navbar/script.js/script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const navbar = document.querySelector(".navbar");
-
-toggleBtn.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
