b150bbae52b7153a71a1d70625c7524fc565f97c
JuanSMartinez <juan.martinez@genstudents.org>
2025-05-22 16:41:12 -0500

    íconos usuarios y comportamiento ventanas de RRSS

--- footer/script/script.js
+++ footer/script/script.js
@@ -1,1 +1,12 @@
+  const form = document.getElementById('form-submit');
+  const message = document.getElementById('mensaje-newsletter');
 
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    message.textContent = "¡Gracias por unirte! Muy pronto recibirás novedades que honran nuestras raíces.";
+    message.style.color = "#789064";
+    message.style.fontWeight = "500";
+
+    // Opcional: limpiar el campo de correo
+    form.reset();
+  });
